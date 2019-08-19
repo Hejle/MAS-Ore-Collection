8bfdd77d133a13786148e885c900844eaaf60bed
Hejle <joachim-dk@hotmail.com>
2019-08-19 13:49:45 +0200

    Added Bare Agentfiles

---
+++ Base.lua
@@ -0,0 +1,26 @@
+Move = require "ranalib_movement"
+Collision = require "ranalib_collision"
+Agent = require "ranalib_agent"
+Event =  require "ranalib_event"
+Variables = require "Variables"
+Constants = require "Constants"
+
+--parameters
+Counter = 0
+
+
+function InitializeAgent()
+
+end
+
+
+function TakeStep()
+end
+
+function HandleEvent(event)
+
+end
+
+function CleanUp()
+
+end
