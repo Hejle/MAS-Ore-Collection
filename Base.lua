62f3d805ecdd3a0135f4b2293eb1581d557e4c59
Ant_bp <antonibepe@gmail.com>
2019-08-20 18:28:24 +0200

    first deployment points selection completed

--- Base.lua
+++ Base.lua
@@ -1,7 +1,7 @@
 Move = require "ranalib_movement"
 Collision = require "ranalib_collision"
 Agent = require "ranalib_agent"
-Event =  require "ranalib_event"
+Event = require "ranalib_event"
 Variables = require "Libs/Variables"
 Constants = require "Libs/Constants"
 SharedPosition = require "Libs/SharedPosition"
@@ -10,36 +10,41 @@ Inspect = require "Libs/inspect"
 --parameters
 Counter = 0
 DeployPositionsList = {}
+ExplorerPose = {}
 
 
 function InitializeAgent()
-    SharedPosition.StoreInformation(ID, {PositionX,PositionY})
-    Agent.changeColor{id=ID, r=128,g=0,b=128}
+    SharedPosition.StoreInformation(ID, {PositionX, PositionY})
+    Agent.changeColor{id = ID, r = 128, g = 0, b = 128}
     GenerateDeployPositions()
+
 end
 
 
 function TakeStep()
     if Counter == 0 then
         InitRobots()
+    
     end
     Counter = Counter + 1
 end
 
-function handleEvent(sourceX, sourceY, sourceID, eventDescription, eventTable)	
+function handleEvent(sourceX, sourceY, sourceID, eventDescription, eventTable)
     
-
     if eventDescription == "deployPositionRequested" and ID ~= sourceID then
-        l_print("Base: " .. ID .. " , Explorer: " .. sourceID .. " has requested a deploy position." )
-        PosExplorer  = table.remove( DeployPositionsList, 1)
-        --say(Inspect.inspect(DeployPositionsList))
-		Event.emit {speed = 343, description = "servingDeployPosition", table = PosExplorer, targetID = sourceID}	
-	end
+        l_print("Base: " .. ID .. " , Explorer: " .. sourceID .. " has requested a deploy position.")
+        ExplorerPose = table.remove(DeployPositionsList, 1)
+        if ExplorerPose ~= nil then
+        Event.emit{speed = 343, description = "servingDeployPosition", table = {position = {ExplorerPose[1], ExplorerPose[2]}, orientation = ExplorerPose[3]}, targetID = sourceID}
+        else
+        Event.emit{speed = 343, description = "servingDeployPosition", table = {position = {PositionX, PositionY}, orientation = "nil"}, targetID = sourceID}
+        end
+    end
 
 end
 
 function InitRobots()
-    Event.emit {speed = 343, description = "init", table = {group=ID, BasePosition={PositionX, PositionY}}, groupID = ID}
+    Event.emit{speed = 343, description = "init", table = {group = ID, BasePosition = {PositionX, PositionY}}, groupID = ID}
 end
 
 function CleanUp()
@@ -47,27 +52,179 @@ function CleanUp()
 end
 
 
-
-function  GenerateDeployPositions()
+-- Sorry for this  collection of fors
+function GenerateDeployPositions()
     
-    PosY = PositionY + Variables.P
-    if PosY > ENV_HEIGHT then
-        PosY = PosY - ENV_HEIGHT
-    end
-    PosExplorer = {PosX,}
+    NumPointsTops = 2
+    NumPointsSides = 2
 
-    for i=1, Variables.X do
-        if i % 2 == 0 then
-            PosX = PositionX + 10* i
-        else
-            PosX = PositionX - 10* i
-        end
-        if PosX > ENV_WIDTH then
-            PosX = PosX - ENV_WIDTH
-        end
-        PosExplorer = {PosX,PositionY + Variables.P}
-        
-        table.insert(DeployPositionsList,i,PosExplorer)
+    offset = Variables.W*NumPointsSides*2 + Variables.W*2
+
+    refX = PositionX
+    refY = PositionY
+
+    for i = 0, (NumPointsTops+1) do
+        PosX = refX + Variables.W * 2 * i
+        PosY = refY + Variables.Z + offset
+        ExplorerPose = {PosX, PosY, "North"}
+        addPose(ExplorerPose)
+    end
+    
+    refX = PositionX - Variables.W
+    for i = 0, NumPointsTops do
+        PosX = refX - Variables.W * 2 * i
+        PosY = refY + Variables.Z*2 + offset
+        ExplorerPose = {PosX, PosY, "North"}
+        addPose(ExplorerPose)
     end
 
+    refX = PositionX + Variables.W
+
+    for i = 0, NumPointsTops do
+        PosX = refX + Variables.W * 2 * i
+        PosY = refY + Variables.Z*2 + offset
+        ExplorerPose = {PosX, PosY, "North"}
+        addPose(ExplorerPose)
+    end
+
+    refX = PositionX - 2*Variables.W
+
+    for i = 0, NumPointsTops do
+        PosX = refX - Variables.W * 2 * i
+        PosY = refY + Variables.Z + offset
+        ExplorerPose = {PosX, PosY, "North"}
+        addPose(ExplorerPose)
+    end
+
+    refX = PositionX
+    refY = PositionY
+
+    for i = 0, (NumPointsTops+1) do
+        PosX = refX + Variables.W * 2 * i
+        PosY = refY - Variables.Z - offset
+        ExplorerPose = {PosX, PosY, "South"}
+        addPose(ExplorerPose)
+    end
+    
+    refX = PositionX - Variables.W
+    for i = 0, NumPointsTops do
+        PosX = refX - Variables.W * 2 * i
+        PosY = refY - Variables.Z*2 - offset
+        ExplorerPose = {PosX, PosY, "South"}
+        addPose(ExplorerPose)
+    end
+
+    refX = PositionX + Variables.W
+
+    for i = 0, NumPointsTops do
+        PosX = refX + Variables.W * 2 * i
+        PosY = refY - Variables.Z*2 - offset
+        ExplorerPose = {PosX, PosY, "South"}
+        addPose(ExplorerPose)
+    end
+
+    refX = PositionX - 2*Variables.W
+
+    for i = 0, NumPointsTops do
+        PosX = refX - Variables.W * 2 * i
+        PosY = refY - Variables.Z - offset
+        ExplorerPose = {PosX, PosY, "South"}
+        addPose(ExplorerPose)
+    end
+
+    refX = PositionX
+    refY = PositionY
+
+    for i = 0, (NumPointsSides+1) do
+        PosY = refY + Variables.Z * 2 * i
+        PosX = refX + Variables.W 
+        ExplorerPose = {PosX, PosY, "East"}
+        addPose(ExplorerPose)
+    end
+    
+    refY = PositionY - Variables.W
+
+    for i = 0, NumPointsTops do
+        PosY = refY - Variables.Z * 2 * i
+        PosX = refX + Variables.W*2 
+        ExplorerPose = {PosX, PosY, "East"}
+        addPose(ExplorerPose)
+    end
+
+    refY = PositionY + Variables.W
+
+    for i = 0, NumPointsTops do
+        PosY = refY + Variables.Z * 2 * i
+        PosX = refX + Variables.W*2 
+        ExplorerPose = {PosX, PosY, "East"}
+        addPose(ExplorerPose)
+    end
+
+    refY = PositionY - 2*Variables.W
+
+    for i = 0, NumPointsTops do
+        PosY = refY - Variables.Z * 2 * i
+        PosX = refX + Variables.W 
+        ExplorerPose = {PosX, PosY, "East"}
+        addPose(ExplorerPose)
+    end
+
+    refX = PositionX
+    refY = PositionY
+
+    for i = 0, (NumPointsTops+1) do
+        PosY = refY + Variables.Z * 2 * i
+        PosX = refX - Variables.W 
+        ExplorerPose = {PosX, PosY, "West"}
+        addPose(ExplorerPose)
+    end
+    
+    refY = PositionY - Variables.W
+
+    for i = 0, NumPointsTops do
+        PosY = refY - Variables.Z * 2 * i
+        PosX = refX - Variables.W*2 
+        ExplorerPose = {PosX, PosY, "West"}
+        addPose(ExplorerPose)
+    end
+
+    refY = PositionY + Variables.W
+
+    for i = 0, NumPointsTops do
+        PosY = refY + Variables.Z * 2 * i
+        PosX = refX - Variables.W*2 
+        ExplorerPose = {PosX, PosY, "West"}
+        addPose(ExplorerPose)
+    end
+
+    refY = PositionY - 2*Variables.W
+
+    for i = 0, NumPointsTops do
+        PosY = refY - Variables.Z * 2 * i
+        PosX = refX - Variables.W 
+        ExplorerPose = {PosX, PosY, "West"}
+        addPose(ExplorerPose)
+    end
+   
+end
+
+function addPose(pose)
+    
+    if pose[1] > ENV_WIDTH then
+        pose[1] = pose[1] - ENV_WIDTH
+    end
+
+    if pose[1] < 0 then
+        pose[1] =  ENV_WIDTH + pose[1]
+    end
+    
+    if pose[2] > ENV_HEIGHT then
+        pose[2] = pose[2] - ENV_HEIGHT
+    end
+
+    if pose[2] < 0 then
+        pose[2] =  ENV_HEIGHT + pose[2]
+    end
+   
+    table.insert(DeployPositionsList, ExplorerPose)
 end
--- Explorer.lua
+++ Explorer.lua
@@ -1,7 +1,7 @@
 Move = require "ranalib_movement"
 Collision = require "ranalib_collision"
 Agent = require "ranalib_agent"
-Event =  require "ranalib_event"
+Event = require "ranalib_event"
 Variables = require "Libs.Variables"
 Constants = require "Libs.Constants"
 SharedPosition = require "Libs.SharedPosition"
@@ -14,6 +14,7 @@ Inspect = require "Libs.inspect"
 Counter = 0
 Group_ID = 0
 TargetPosition = {PositionX, PositionY}
+TargetOrientation = ""
 BasePosition = {}
 TotalMemory = Variables.S
 UsedMemory = 0
@@ -22,73 +23,76 @@ MyState = State.Base
 
 
 function InitializeAgent()
-    SharedPosition.StoreInformation(ID, {PositionX,PositionY})
-    TargetPosition = {PositionX,PositionY}
-    
+    SharedPosition.StoreInformation(ID, {PositionX, PositionY})
+    TargetPosition = {PositionX, PositionY}
+
+
+
+
+
 end
 
-function handleEvent(sourceX, sourceY, sourceID, eventDescription, eventTable)	
-
+function handleEvent(sourceX, sourceY, sourceID, eventDescription, eventTable)
+    
     if eventDescription == "init" and ID ~= sourceID then
-       if(eventTable ~= nil) then
-        Group_ID = eventTable["group"]
-        BasePosition = eventTable["BasePosition"]
-        l_print("Explorer: " .. ID .. " has Group_ID: " .. Group_ID )
-       else
-        l_print("ERROR: eventable empty." )
-       end
-
-
-	elseif eventDescription == "servingDeployPosition" and ID ~= sourceID then
-        l_print("Explorer: " .. ID .. " has recieved a deploy position, from Base: " .. sourceID )
-        TargetPosition  = eventTable
+        if (eventTable ~= nil) then
+            Group_ID = eventTable["group"]
+            BasePosition = eventTable["BasePosition"]
+            l_print("Explorer: " .. ID .. " has Group_ID: " .. Group_ID)
+        else
+            l_print("ERROR: eventable empty.")
+        end
+    
+    elseif eventDescription == "servingDeployPosition" and ID ~= sourceID then
+        l_print("Explorer: " .. ID .. " has recieved a deploy position, from Base: " .. sourceID)
+        TargetPosition = eventTable["position"]
+        TargetOrientation = eventTable["orientation"]
         say(Inspect.inspect(TargetPosition))
         MyState = State.Deploying
-	end
+    end
 end
 
 function TakeStep()
-
+    
     if Counter == 0 then
-        Event.emit {speed = 343, description = "deployPositionRequested",targetID = Group_ID}	
+        Event.emit{speed = 343, description = "deployPositionRequested", targetID = Group_ID}
     end
-
+    
     if UsedMemory == TotalMemory then
         TargetPosition = BasePosition
     end
-
+    
     Counter = Counter + 1
     
-    --Search()
+    
+    
     --SharedPosition.StoreInformation(ID, {PositionX,PositionY})
-
     if PositionX ~= TargetPosition[1] or PositionY ~= TargetPosition[2] then
-        Utilities.moveTorus(TargetPosition[1],TargetPosition[2])
+        Utilities.moveTorus(TargetPosition[1], TargetPosition[2])
     else
         if MyState == State.Deploying then
             MyState = State.Deployed
+            
+        end
+        
+        if MyState == State.Deployed then
+            Search()
+            getNextStep()
+            
         end
     end
 end
 
 function Search()
-	local table = Map.radialMapColorScan(Variables.P, Constants.ore_color[1],  Constants.ore_color[2],  Constants.ore_color[3])
-
+    local table = Map.radialMapColorScan(Variables.P, Constants.ore_color[1], Constants.ore_color[2], Constants.ore_color[3])
+    
     if table ~= nil then
-        for i=1, #table do
-            Map.quantumModify(table[i].posX, table[i].posY,Constants.ore_color, Constants.ore_color_found)
+        for i = 1, #table do
+            Map.quantumModify(table[i].posX, table[i].posY, Constants.ore_color, Constants.ore_color_found)
             AddInfoToMemory({table[i].posX, table[i].posY})
         end
-        
-	end
-	--if no grass is found search a new area
-	--direction = {
-	--	PositionX + l_getRandomInteger(-Variables.P, Variables.P),
-	--	PositionY + l_getRandomInteger(-Variables.P, Variables.P)
-	--}
-    --Move.to {x = 100, y = 100, speed = 10}
     
-
+    end
 end
 
 
@@ -97,25 +101,81 @@ function CleanUp()
 
 end
 
+function getNextStep()
+    
+    if TargetOrientation == "North" then
+        
+        TargetPosition = {PositionX, PositionY + 1}
+        Agent.changeColor{r=0, g=255, b=0}
+    
+    elseif TargetOrientation == "South" then
+        
+        TargetPosition = {PositionX, PositionY - 1}
+        Agent.changeColor{r=255, g=255, b=255}
+    
+    elseif TargetOrientation == "East" then
+        
+        TargetPosition = {PositionX + 1, PositionY}
+        Agent.changeColor{r=0, g=0, b=255}
+    
+    elseif TargetOrientation == "West" then
+        
+        TargetPosition = {PositionX - 1, PositionY}
+    
+    elseif TargetOrientation == "NorthWest" then
+        
+        TargetPosition = {PositionX - 1, PositionY + 1}
+    
+    elseif TargetOrientation == "SouthWest" then
+        
+        TargetPosition = {PositionX - 1, PositionY - 1}
+
+    elseif TargetOrientation == "NorthEast" then
+        
+        TargetPosition = {PositionX + 1, PositionY + 1}
+
+    elseif TargetOrientation == "SouthEast" then
+        
+        TargetPosition = {PositionX + 1, PositionY - 1}
+    end  
+    
+    if TargetPosition[1] >= ENV_WIDTH then
+        TargetPosition[1] = TargetPosition[1] - ENV_WIDTH
+    end
+
+    if TargetPosition[1] < 0 then
+        TargetPosition[1] =  ENV_WIDTH + TargetPosition[1]
+    end
+    
+    if TargetPosition[2] >= ENV_HEIGHT then
+        TargetPosition[2] = TargetPosition[2] - ENV_HEIGHT
+    end
+
+    if TargetPosition[2] < 0 then
+        TargetPosition[2] =  ENV_HEIGHT + TargetPosition[2]
+    end
+
+end
+
 function AddInfoToMemory(info)
     if UsedMemory ~= TotalMemory then
-          table.insert(Memory, info)
-          UsedMemory = UsedMemory + 1
+        table.insert(Memory, info)
+        UsedMemory = UsedMemory + 1
     end
 end
 
 function RemoveInfoFromMemory(index)
     if Memory[index] ~= nil then
-          table.remove(Memory, index)
-          UsedMemory = UsedMemory - 1
+        table.remove(Memory, index)
+        UsedMemory = UsedMemory - 1
     end
 end
 
 function AlterInfoFromMemory(info, index)
     if Memory[index] ~= nil then
-          table.insert(Memory, index, info)
+        table.insert(Memory, index, info)
     else
-          AddInfoToMemory(index, info)
+        AddInfoToMemory(index, info)
     end
 end
 
